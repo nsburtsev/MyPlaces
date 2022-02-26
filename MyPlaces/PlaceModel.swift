7369ec94d480e700569f2bfafa0beb7eb9422321
Nyurgun <burtsev.ns@gmail.com>
2022-02-26 19:00:22 +0900

    Work with model

---
+++ MyPlaces/PlaceModel.swift
@@ -0,0 +1,47 @@
+//
+//  PlaceModel.swift
+//  MyPlaces
+//
+//  Created by Нюргун on 26.02.2022.
+//  Copyright © 2022 Нюргун. All rights reserved.
+//
+
+import Foundation
+
+struct Place {
+    
+    var name: String
+    var location: String
+    var type: String
+    var image: String
+    
+    static let restaurantNames = [
+        "Burger Heroes",
+        "Kitchen",
+        "Bonsai",
+        "Дастархан",
+        "Индокитай",
+        "X.O",
+        "Балкан Гриль",
+        "Sherlock Holmes",
+        "Speak Easy",
+        "Morris Pub",
+        "Вкусные истории",
+        "Классик",
+        "Love&Life",
+        "Шок",
+        "Бочка"
+    ]
+    
+    static func getPlaces() -> [Place] {
+        
+        var places = [Place]()
+        
+        for place in restaurantNames {
+            places.append(Place(name: place, location: "Уфа", type: "Ресторан", image: place))
+        }
+        
+        return places
+    }
+    
+}
